f47b853294f4d9617e0a5da62a7ac2aefe63111f
vadimm <vmelnikov@baltinfocom.ru>
2021-11-14 10:50:24 +0300

    Add routing

--- Evee-webapp/src/app/app.module.ts
+++ Evee-webapp/src/app/app.module.ts
@@ -1,12 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { Routes, RouterModule } from '@angular/router';
+
 import { AppComponent } from './app.component';
 import { PokemonComponent } from 'src/app/pokemon/pokemon.component';
 import { HoverDirective } from 'src/app/hover.directive';
 import { LoginComponent } from 'src/app/login/login.component';
 import { HomeComponent } from 'src/app/home/home.component';
 import { MaskedDirective } from 'src/app/masked.directive';
+
+const appRoutes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'pokemon',
+    component: PokemonComponent,
+  },
+];
 
 @NgModule({
   declarations: [
@@ -20,6 +37,7 @@ import { MaskedDirective } from 'src/app/masked.directive';
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [],
   bootstrap: [AppComponent],
